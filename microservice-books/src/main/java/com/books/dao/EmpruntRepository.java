86580e01dcd46e07f6ad3af8013054c46ebb0181
Paul <psebire37@gmail.com>
2020-09-07 17:53:45 +0200

    added reserver button and no reservation if already borrowed

--- microservice-books/src/main/java/com/books/dao/EmpruntRepository.java
+++ microservice-books/src/main/java/com/books/dao/EmpruntRepository.java
@@ -5,6 +5,7 @@ package com.books.dao;
 import com.books.entities.Emprunt;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Date;
 import java.util.List;
@@ -15,4 +16,8 @@ public interface EmpruntRepository extends JpaRepository<Emprunt, Long> {
     List<Emprunt> findAllByIdUtilisateurAndCloturerFalseOrderByIdAsc(long idUser);
     List<Emprunt> findAllByCopy_BookIdAndCloturerIsFalseOrderByDateRetourAsc(long id);
     Set<Emprunt> findByCloturerFalseAndDateRetourBefore(Date date);
+
+    @Query(value = "select e from Emprunt e inner join fetch e.copy c where e.idUtilisateur=:idUser and e.cloturer=false and c.book.id=:idBook",
+            countQuery = "select count (e) from Emprunt e inner join e.copy c where e.idUtilisateur=:idUser and e.cloturer=false and c.book.id=:idBook")
+    List<Emprunt> livreDejaEmprunteParUtilisateur(@Param("idUser")long idUSer, @Param("idBook")long idBook);
 }
